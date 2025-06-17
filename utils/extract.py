4b3da7c5b63efec3460358c89cd2369b054d7af7
umartils <umartilmisani48@gmail.com>
2025-06-17 22:18:31 +0700

    add fetching_content funct

--- utils/extract.py
+++ utils/extract.py
@@ -2,3 +2,14 @@ import time
 import requests
 from bs4 import BeautifulSoup
 from datetime import datetime
+
+def fetching_content(url):
+    HEADERS = {"User-Agent": "Chrome/96.0.4664.110"}
+    try:
+        session = requests.Session()
+        response = session.get(url, headers=HEADERS)
+        response.raise_for_status()
+        return response.content
+    except Exception as e:
+        print(f"Terjadi kesalahan ketika melakukan requests terhadap {url}: {e}")
+        return None
