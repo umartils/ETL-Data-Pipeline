71a012287a3acb1b0d38dc2a99bc7a003d4669fc
umartils <umartilmisani48@gmail.com>
2025-06-17 22:21:00 +0700

    edit utils/extract.py files

--- utils/extract.py
+++ utils/extract.py
@@ -14,4 +14,14 @@ def fetching_content(url):
         print(f"Terjadi kesalahan ketika melakukan requests terhadap {url}: {e}")
         return None
 
-def extract_products_data():
+def extract_products_data(products):
+    try:
+        title = products.find('h3', class_='product-title').text.strip()
+    except AttributeError:
+        title = "No title found"
+
+    try:
+        price_element = products.find('div', class_='price-container')
+        price = price_element.find('span', class_='price').text.strip() if price_element else "Price unavailable"
+    except AttributeError:
+        price = "Price not available"
