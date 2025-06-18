279f553321113539492bfb5929364f839161b0a7
umartils <umartilmisani48@gmail.com>
2025-06-18 08:29:36 +0700

    add new function

--- utils/extract.py
+++ utils/extract.py
@@ -25,3 +25,50 @@ def extract_products_data(products):
         price = price_element.find('span', class_='price').text.strip() if price_element else "Price unavailable"
     except AttributeError:
         price = "Price not available"
+    
+    paragraphs = products.find_all('p', style="font-size: 14px; color: #777;")
+
+    rating = paragraphs[0].text
+    color = paragraphs[1].text
+    size = paragraphs[2].text
+    gender = paragraphs[3].text
+
+    fashion = {
+        "Title": title,
+        "Price": price,
+        "Rating": rating,
+        "Colors": color,
+        "Size": size,
+        "Gender": gender,
+        "Time": datetime.now().isoformat()
+    }
+
+    return fashion
+def products_scraping(base_url, start_page=1, end_page=50, delay=2):
+    data = []
+
+    for page_number in range(start_page, end_page + 1):
+        # print(f"Scraping halaman {page_number}: {url}")
+        if page_number == 1:
+            url = base_url.rstrip('{}')
+        else:
+            url = base_url.format(f"page{page_number}")
+        content = fetching_content(url)
+        if not content:
+            print(f"Gagal mengambil konten dari halaman {page_number}")
+            continue
+
+        soup = BeautifulSoup(content, "html.parser")
+        elements = soup.find_all('div', class_='collection-card')
+
+        if not elements:
+            print(f"Tidak ada data ditemukan di halaman {page_number}")
+            break
+
+        for products in elements:
+            fashion = extract_products_data(products)
+            data.append(fashion)
+
+        time.sleep(delay)
+
+    return data
